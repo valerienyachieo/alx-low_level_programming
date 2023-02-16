8e20239694064a079e64312e90b2843bda1f34fe
valerienyachieo <valerienyachieo@gmail.com>
2023-02-16 14:53:58 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+/**
+ * main - entry point
+ * return: Always o (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)")
+	printf("Size of an int: %lu byte(s)")
+	printf("Size of a long int: %lu byte(s)")
+	printf("Size of a long long int: %lu byte(s)")
+	printf("Size of a float: %lu byte(s)")
+	return (0)
+}
