bca18f1886a93414f4c20cebffe83b070b173a5a
valerienyachieo <valerienyachieo@gmail.com>
2023-02-19 07:04:21 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,12 +9,13 @@ int main(void)
 	char a;
 
 	a = 'a';
-	while (a <= 'z')
-	{
-	       if ((a != 'e') && (a != 'q'))
-		       putchar(a);
-		a++;
-	}
+	while 
+		(a <= 'z')
+		{
+			if ((a != 'e') && (a != 'q'))
+			       	putchar(a);
+			a++;
+		}	
 	putchar('\n');
 	return (0);
 }
