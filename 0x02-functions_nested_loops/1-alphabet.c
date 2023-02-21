60c13f9b36fc7e1a3fd9bf997d9d4a971bddeff0
valerienyachieo <valerienyachieo@gmail.com>
2023-02-21 14:03:57 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabet in lower case
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		_putchar(x);
+	_putchar('\n');
+	return (0);
+}
