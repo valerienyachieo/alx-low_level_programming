8949cddeaa48afae8115314631d0c9d84c47eed7
valerie nyachieo <valerienyachieo@gmail.com>
2023-02-22 06:58:56 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - function that computes absolute value of an interger
+ *
+ * @n: interger to be checked
+ *
+ * Return: always n.
+ */
+int _abs(int n)
+{
+	return (n);
+}
