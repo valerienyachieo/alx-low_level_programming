ed90a08f2f44795486357c97ada6c4a942b13526
valerie nyachieo <valerienyachieo@gmail.com>
2023-02-22 11:25:17 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,10 +15,23 @@ void times_table(void)
 		for (j = 0; j < 9; j++)
 		{
 			k = i * j;
-			_putchar(',');
-			_putchar(' ');
-			_putchar(k / 10 + '0');
-			_putchar(k % 10 + '0');
+			if (j == 0)
+			{
+				_putchar(k + '0');
+			}
+			if (k < 10 && i != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(k + '0');
+			}
+			else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(k / 10 + '0');
+				_putchar(k % 10 + '0');
+			}
 		}
 		_putchar('\n');
 	}
