a5d34e4c0bd8479e9484a27535a70151f4628e3d
valerie nyachieo <valerienyachieo@gmail.com>
2023-02-22 15:21:48 -0800

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_times_table - function that n times table starting with zero.
+ *
+ * @n: function parameter
+ *
+ * Return: n times table.
+ */
+void print_times_table(int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= n i++)
+	{
+		for (j = 0; j <= n; j++)
+			n = (j * i);
+
+
+
--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -20,16 +20,18 @@ void print_to_98(int n)
 			else
 				printf("%d", i);
 		}
+		printf("\n");
 	}
 	else if (n > 98)
 	{
-		for (i = n; i > 98; i--)
+		for (i = n; i >= 98; i--)
 		{
 			if (i != 98)
 				printf("%d, ", i);
 			else
 				printf("%d", i);
 		}
+		printf("\n");
 	}
 	else if (n < 0)
 	{
@@ -40,5 +42,6 @@ void print_to_98(int n)
 			else
 				printf("%d", i);
 		}
+		printf("\n");
 	}
 }
