ec37f8b049a7b8a06e197751cd30d9e1ea8908ae
valerie nyachieo <valerienyachieo@gmail.com>
2023-02-23 13:43:31 -0800

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - function that prints the number of times line is printed.
+ * @n: function parameter
+ *
+ * Return: no return
+ */
+void print_line(int n)
+{
+	int a;
+
+	for (a = 0; a < n; a++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
