379cb614695ea8d2918b1a7e412d12c5168524f6
valerie nyachieo <valerienyachieo@gmail.com>
2023-02-23 11:31:00 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - function that multiplies intergers.
+ * @a: function parameter
+ * @b: function parameter
+ *
+ * Return: results
+ */
+int mul(int a, int b)
+{
+	int results;
+
+	results = a * b;
+	return (results);
+}
