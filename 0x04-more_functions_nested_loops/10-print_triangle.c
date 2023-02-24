93d60c57b68be0f006c192ab53d71256d06cebe1
valerie nyachieo <valerienyachieo@gmail.com>
2023-02-23 16:40:15 -0800

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_triangle - function that prints triangle.
+ * @size: function parameter
+ *
+ * Return: No return
+ */
+void print_triangle(int size)
+{
+	int a;
+	int b;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = a; b < size; b++)
+			{
+				_putchar(' ');
+			}
+			for (b = 0; b <= a; b++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
