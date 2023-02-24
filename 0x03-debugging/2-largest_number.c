6539753d15c52ec4011d6345fa40ab0cfca7ff22
valerie nyachieo <valerienyachieo@gmail.com>
2023-02-24 12:14:30 -0800

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,23 +13,23 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a > b && b >= c)
 	{
 		largest = a;
 	}
-	else if (b > a && a > c)
+	else if (b > a && a >= c)
 	{
 		largest = b;
 	}
-	else if (c > a && a > b)
+	else if (c > a && a >= b)
 	{
 		largest = c;
 	}
-	else if (a > c && c > b)
+	else if (a > c && c >= b)
 	{
 		largest = a;
 	}
-	else if (b > c && c > a)
+	else if (b > c && c >= a)
 	{
 		largest = b;
 	}
