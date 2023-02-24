73c4bfff7061f0ff21d66d95e8f2ad2ea458ae53
valerie nyachieo <valerienyachieo@gmail.com>
2023-02-23 16:00:18 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,11 +10,11 @@ int main(void)
 
 	for (a = 1; a <= 100; a++)
 	{
-		if ((a % 3 == 0) && (a % 3 != 5))
+		if ((a % 3 == 0) && (a % 5 != 0))
 		{
 			printf(" Fizz");
 		}
-		else if ((a % 5 == 0) && (a != 3))
+		else if ((a % 5 == 0) && (a % 3 !=  0))
 		{
 			printf(" Buzz");
 		}
