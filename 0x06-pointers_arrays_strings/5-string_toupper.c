c0b92c7f74219e7e4d1487c3f8a1dece75ff7a20
valerie nyachieo <valerienyachieo@gmail.com>
2023-03-02 11:20:59 -0800

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - function that changes string lowercase letters to uppercase
+ * @s: function parameter
+ *
+ * Return: s
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+	}
+	return (s);
+}
