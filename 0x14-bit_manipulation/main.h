2b213ba66306b4feb9be4e0982fafcb83d6e7ba7
valerie nyachieo <valerienyachieo@gmail.com>
2023-04-06 16:30:17 -0700

    Header File

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
