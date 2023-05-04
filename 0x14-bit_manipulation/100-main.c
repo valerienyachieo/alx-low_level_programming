a14eb6a5879978ad0eac6ebab5137d5f92b1cd44
valerienyachieo <valerienyachieo@gmail.com>
2023-05-04 12:10:12 -0700

    checking the endianness

---
+++ 0x14-bit_manipulation/100-main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - main function
+ *
+ * Return: no return
+ */
+int main(void)
+{
+	int n;
+
+	n = get_endianness();
+	if (n != 0)
+	{
+		printf("Little Endian\n");
+	}
+	else
+	{
+		printf("Big Endian\n");
+	}
+	return (0);
+}
